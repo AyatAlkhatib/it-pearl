077dc0c71fabdfe854df24189b909f1f0034eb81
AyatAlkhatib <142817899+AyatAlkhatib@users.noreply.github.com>
2025-02-21 03:47:35 +0000

    worked on unit conversion

--- unitsconversion/assest/js/script.js
+++ unitsconversion/assest/js/script.js
@@ -4,134 +4,114 @@ $("#myform").validate({
 
 function calculate(){
     "use strict"
-     if ($("#myform").valid()){
-        let fromvalue = document.getElementById("fromvalue").value;
-        let tovalue = document.getElementById("tovalue").value;
+    let form = $("#myform");
 
-        let fromvaluefp = parseFloat(fromvalue);
-        let tovaluefp = parseFloat(tovalue);
 
-        let value;
-            if (document.getElementById("fromcentimenters").checked){
-                value = "cm";
-        }
-        if (document.getElementById("frommmeters").checked){
-            value = "m";
-        }
+     if (form.valid()){
 
-        if (document.getElementById("fromkilometers").checked){
-            value = "km";
-        }
+        let Fromvalue = document.getElementById("fromvalue").value;
+        let Fromunit = "";
 
-        if (document.getElementById("frominches").checked){
-            value = "in";
+    
+        if (document.getElementById("fromcentimenters").checked){
+            Fromunit = "cm";
         }
-
-        if (document.getElementById("fromfeet").checked){
-            value = "ft";
+            else if (document.getElementById("frommmeters").checked){
+                Fromunit = "m";
         }
-        if (document.getElementById("fromyards").checked){
-            value = "yd";
-        }
-        if (document.getElementById("frommiles").checked){
-            value = "mi";
-        }
-
-         let value;
-            if (document.getElementById("fromcentimenters").checked){
-                value = "cm";
-        }
-        if (document.getElementById("frommmeters").checked){
-            value = "m";
-        }
-
-        if (document.getElementById("fromkilometers").checked){
-            value = "km";
-        }
-
-        if (document.getElementById("frominches").checked){
-            value = "in";
-        }
-
-        if (document.getElementById("fromfeet").checked){
-            value = "ft";
-        }
-        if (document.getElementById("fromyards").checked){
-            value = "yd";
-        }
-        if (document.getElementById("frommiles").checked){
-            value = "mi";
-        }
-
-         let value;
-            if (document.getElementById("fromcentimenters").checked){
-                value = "cm";
-        }
-        if (document.getElementById("frommmeters").checked){
-            value = "m";
-        }
-
-        if (document.getElementById("fromkilometers").checked){
-            value = "km";
-        }
-
-        if (document.getElementById("frominches").checked){
-            value = "in";
-        }
-
-        if (document.getElementById("fromfeet").checked){
-            value = "ft";
-        }
-        if (document.getElementById("fromyards").checked){
-            value = "yd";
-        }
-        if (document.getElementById("frommiles").checked){
-            value = "mi";
-        } 
-        let tovalue;
-        if (document.getElementById("tocentimenters").checked){
-            value = "cm";
-        }
-        if (document.getElementById("tometers").checked){
-        value = "m";
-        }
-
-        if (document.getElementById("tokilometers").checked){
-        value = "km";
-        }
-
-        if (document.getElementById("toinches").checked){
-            value = "in";
-        }
-
-        if (document.getElementById("tofeet").checked){
-            value = "ft";
-        }
-        if (document.getElementById("toyards").checked){
-            value = "yd";
-        }
-        if (document.getElementById("tomiles").checked){
-            value = "mi";
-        }
-
-        let result;
-
-            if (operator == "cm"){
-                result = fromvaluefp + operand2fp;
-            }
-            else if (operator == "minus"){
-                result = operand1fp - operand2fp;
-            }
-            else if (operator == "multiply"){
-                result = operand1fp * operand2fp;
-            }
-            else if (operator == "divided"){
-                result = operand1fp / operand2fp;
-            }
-
-            document.getElementById("Result").innerHTML = result.toString();
-
         
+            else if (document.getElementById("fromkilometers").checked){
+                Fromunit = "km";
+        }
 
+            else if (document.getElementById("frominches").checked){
+                Fromunit = "in";
+        }
+
+            else if (document.getElementById("fromfeet").checked){
+                Fromunit = "ft";
+        }
+            else if (document.getElementById("fromyards").checked){
+                Fromunit = "yd";
+        }
+            else if (document.getElementById("frommiles").checked){
+                Fromunit = "mi";
+        }
+        
+        let Tounit="";
+
+        if (document.getElementById("tocentimenters").checked){
+            Tounit = "cm";
+        }
+            else if (document.getElementById("tometers").checked){
+                Tounit = "m";
+        }
+
+            else if (document.getElementById("tokilometers").checked){
+                Tounit = "km";
+        }
+
+            else if (document.getElementById("toinches").checked){
+                Tounit = "in";
+        }
+
+            else if (document.getElementById("tofeet").checked){
+                Tounit = "ft";
+        }
+            else if (document.getElementById("toyards").checked){
+                Tounit = "yd";
+        }
+            else if (document.getElementById("tomiles").checked){
+                Tounit = "mi";
+        } 
+        
+        ConvertUnits(Fromvalue,Fromunit, Tounit);
      }
 }
+
+        async function ConvertUnits(Fromvalue, Fromunit, Tounit){
+            "use strict";
+            let bruceUrl = " https://brucebauer.info/assets/ITEC3650/unitsconversion.php";
+            bruceUrl= bruceUrl+ "?FromValue=" + encodeURIComponent(Fromvalue) + "&FromUnit=" + encodeURIComponent(Fromunit) + "&ToUnit=" + encodeURIComponent(Tounit);
+
+            let respond = await fetch (bruceUrl);
+            let result = await respond.json();
+
+            document.getElementById("tovalue").innerHTML=result;
+
+        }
+
+        function clearform(){
+            "use strict";
+            document.getElementById("fromvalue").value="";
+
+            document.getElementById("cm").checked = false;
+            document.getElementById("m").checked = false;
+            document.getElementById("km").checked = false;
+            document.getElementById("in").checked = false;
+            document.getElementById("ft").checked = false;
+            document.getElementById("yd").checked = false;
+            document.getElementById("mi").checked = false;
+
+            document.getElementById("tocentimenters").checked = false;
+            document.getElementById("tometers").checked = false;
+            document.getElementById("tokilometers").checked = false; 
+            document.getElementById("toinches").checked = false;
+            document.getElementById("tofeet").checked = false; 
+            document.getElementById("toyards").checked = false; 
+            document.getElementById("tomiles").checked = false; 
+
+            document.getElementById("tovalue").innerHTML="";
+
+
+
+        }
+    
+        $("#myform").validate({});
+
+
+
+
+
+        
+  
